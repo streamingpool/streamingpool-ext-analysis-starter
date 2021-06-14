1585cbfbc944fc3179885e902139b25dfe7fd5c3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-14 08:14:01 +0000

    Deploy streamingpool/streamingpool-ext-analysis-starter to github.com/streamingpool/streamingpool-ext-analysis-starter.git:gh-pages

---
+++ javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.streamingpool.ext.analysis.starter","c":"StreamingpoolAnalysisConfiguration","l":"StreamingpoolAnalysisConfiguration()","url":"%3Cinit%3E()"}]
---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.streamingpool.ext.analysis.starter","l":"StreamingpoolAnalysisConfiguration"}]
